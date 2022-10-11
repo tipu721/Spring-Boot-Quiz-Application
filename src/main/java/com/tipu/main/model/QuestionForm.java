83931a14ae3cecaf11ce61d735b6e86f984fb209
tipu721 <tsultan721@gmail.com>
2022-10-11 12:24:10 +0600

    first commit

---
+++ src/main/java/com/tipu/main/model/QuestionForm.java
@@ -0,0 +1,21 @@
+package com.tipu.main.model;
+
+import java.util.List;
+
+public class QuestionForm {
+	
+	private List<Question> questions;
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+	
+
+	
+}
---
+++ src/main/java/com/tipu/main/model/Result.java
@@ -0,0 +1,8 @@
+package com.tipu.main.model;
+
+public class Result {
+
+	private int id;
+	private String username;
+	private int totalCorrect;
+}
